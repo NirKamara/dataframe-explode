ba048f54abd556ae4a6f0fd5169567a9754295cc
NirKamara <nirkamara@gmail.com>
2016-12-28 13:04:23 +0200

    aaa

--- src/main/java/linkedIn/LinkedInService.java
+++ src/main/java/linkedIn/LinkedInService.java
@@ -1,30 +1,69 @@
 package linkedIn;
 
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import scala.Tuple2;
+
+import javax.annotation.PostConstruct;
+
+import static org.apache.spark.sql.functions.*;
 
 /**
- * Created by Evegeny on 28/12/2016.
+ * Created by student12 on 28/12/2016.
  */
 @Service
 public class LinkedInService {
-    @Autowired
-    private SQLContext sqlContext;
 
+    @Autowired
+    private LinkedInDataFrameCreator creator;
+
+    private DataFrame dataFrame;
+
+    @PostConstruct
+    public void initDataFrame() {
+        dataFrame = creator.createDataFrame();
+    }
+
+
+    // Q1
     public void printAll() {
-        DataFrame dataFrame = sqlContext.read().json("data/linkedIn/*.json");
         dataFrame.show();
+
+    }
+
+    // Q2
+    public void printSchema() {
+        dataFrame.printSchema();
+    }
+
+    public void printColumnTypes() {
+
+        Tuple2<String, String>[] columnTypes = dataFrame.dtypes();
+        for (Tuple2<String, String> columnType : columnTypes) {
+            System.out.println("ColumnName: " + columnType._1() + "; ColumnType: " + columnType._2());
+        }
+    }
+
+    public void addSalary() {
+        // DataFrame dataFrame = creator.createDataFrame();
+        dataFrame = dataFrame.withColumn("salary",
+                size(col("keywords"))
+                .multiply(col("age")
+                .multiply(10)));
+
+    }
+
+    public void findDev(){
+        DataFrame dataFrame2 = dataFrame.withColumn("Tech", explode(col("keywords")));
+        Row[] take = dataFrame2.groupBy(col("Tech")).count().orderBy(col("count").desc()).take(1);
+        String mostPopular = take[0].get(0).toString();
+
+        dataFrame = dataFrame
+                .filter("salary>1200")
+                .filter(functions.array_contains(col("keywords"),mostPopular));
     }
 }
-
-
-
-
-
-
-
-
-
-
